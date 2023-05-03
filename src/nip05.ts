37977084b83ff5042c05263d7c4c84fb99f0d14d
Alejandro Gomez <alejandro@strike.me>
2023-05-03 15:52:51 +0200

    rm gzuus temporarily

--- src/nip05.ts
+++ src/nip05.ts
@@ -7,7 +7,6 @@ const handleToPubkey = {
     "7fa56f5d6962ab1e3cd424e758c3002b8665f7b0d8dcee9fe9e288d7751ac194",
   tony: "7f5c2b4e48a0e9feca63a46b13cdb82489f4020398d60a2070a968caa818d75d",
   moon: "5df413d4c5e5035ff508fd99b38b21ea9a0ac0b9ecc34f3312aba9aa2add4f5b",
-  gzuuus: "40b9c85fffeafc1cadf8c30a4e5c88660ff6e4971a0dc723d5ab674b5e61b451",
 };
 
 const pubkeyToHandle = Object.entries(handleToPubkey).reduce((acc, item) => {
