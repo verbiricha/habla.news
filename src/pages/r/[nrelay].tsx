b15cd1337f1d3acc6e0dcf4c86f444a365ed5e47
Alejandro Gomez <alejandro@strike.me>
2023-05-04 18:00:11 +0200

    :fire:

--- src/pages/r/[nrelay].tsx
+++ src/pages/r/[nrelay].tsx
@@ -1,14 +1,11 @@
-import { useState, useMemo } from "react";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 import Head from "next/head";
 
-import { Flex, Heading, Text } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
 
-import { LONG_FORM, HIGHLIGHT } from "@habla/const";
 import { decodeNrelay } from "@habla/nostr";
 import Layout from "@habla/layouts/Layout";
-import RelayFavicon from "@habla/components/RelayFavicon";
 
 const Relay = dynamic(() => import("@habla/components/nostr/Relay"), {
   ssr: false,
@@ -18,10 +15,6 @@ export default function RelayPage() {
   const router = useRouter();
   const { nrelay } = router.query;
   const relay = decodeNrelay(nrelay);
-  const now = useMemo(() => Math.floor(Date.now() / 1000), []);
-  const [since, setSince] = useState(
-    Math.floor((Date.now() - 1 * 24 * 60 * 60 * 1000) / 1000)
-  );
   return (
     <>
       <Head>
