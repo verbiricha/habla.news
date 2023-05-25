33db986120ca1f6baeebeeb68dcd9d6dcdba3b64
Alejandro Gomez <alejandro@strike.me>
2023-05-25 16:50:03 +0200

    missing font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@fontsource/unbounded/400.css";
+import "@fontsource/unbounded/600.css";
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
 import "@fontsource/inter/600.css";
