47126bda6f46e04fe240e59802a6e0625ad81cc9
Alejandro Gomez <alejandro@strike.me>
2023-05-27 10:48:31 +0200

    emoji lists

--- src/const.ts
+++ src/const.ts
@@ -1,23 +1,25 @@
-export const LONG_FORM = 30023;
-export const LONG_FORM_DRAFT = 30024;
-export const HIGHLIGHT = 9802;
-export const RELAYS = 10002;
+// Nostr
 export const NOTE = 1;
 export const CONTACTS = 3;
 export const REPOST = 6;
 export const REACTION = 7;
-export const BADGE = 30009;
 export const ZAP = 9735;
 export const ZAP_REQUEST = 9734;
 export const PROFILE = 0;
+export const HIGHLIGHT = 9802;
 export const MUTED = 10000;
 export const PINNED = 10001;
+export const RELAYS = 10002;
 export const PEOPLE = 30000;
 export const BOOKMARKS = 30001;
+export const LONG_FORM = 30023;
+export const LONG_FORM_DRAFT = 30024;
+export const BADGE = 30009;
+export const EMOJIS = 30030;
 export const ZAPSTR_TRACK = 31337;
 export const ZAPSTR_LIST = 31338;
-export const LISTS = [MUTED, PINNED, PEOPLE, BOOKMARKS, ZAPSTR_LIST];
-
+export const LISTS = [MUTED, PINNED, PEOPLE, BOOKMARKS, ZAPSTR_LIST, EMOJIS];
+// Time
 export const HOUR = 60 * 60;
 export const DAY = 24 * HOUR;
 export const WEEK = 7 * DAY;
