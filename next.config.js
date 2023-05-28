c21b18422fc928376eb6ac095767a05e2a1583a6
Alejandro Gomez <alejandro@strike.me>
2023-05-28 09:46:35 +0200

    add italian

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "es", "ja", "de", "ru", "uk", "fa"],
+    locales: ["en", "es", "ja", "de", "ru", "uk", "fa", "it"],
   },
 
   typescript: {
