fcf5af9ed7fa01f5950ff2aef37a2d0dbd5f3948
Alejandro Gomez <alejandro@strike.me>
2023-05-31 09:28:25 +0200

    filter long tags

--- src/hooks/useTopTags.tsx
+++ src/hooks/useTopTags.tsx
@@ -4,11 +4,16 @@ import { LONG_FORM } from "@habla/const";
 import db from "@habla/cache/db";
 import { pickTopNHashtags } from "@habla/tags";
 
+const TAG_MAX_LENGTH = 32;
+
 export default function useTopTags(n = 10) {
   const tags = useLiveQuery(async () => {
     try {
       const evs = await db.event.where("kind").equals(LONG_FORM).toArray();
-      return evs.map((e) => e.t).flat();
+      return evs
+        .map((e) => e.t)
+        .flat()
+        .filter((t) => t.length <= TAG_MAX_LENGTH);
     } catch (error) {
       console.error(error);
       return [];
