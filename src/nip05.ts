6c7d35eb0c72e5b6fa3523ceafd5fdfaa37409e5
Alejandro Gomez <alejandro@strike.me>
2023-06-14 10:40:11 +0200

    add gek

--- src/nip05.ts
+++ src/nip05.ts
@@ -29,6 +29,7 @@ export const names = {
   karnage: "1bc70a0148b3f316da33fe3c89f23e3e71ac4ff998027ec712b905cd24f6a411",
   herald: "7e7224cfe0af5aaf9131af8f3e9d34ff615ff91ce2694640f1f1fee5d8febb7d",
   jb55: "32e1827635450ebb3c5a7d12c1f8e7b2b514439ac10a67eef3d9fd9c5c68e245",
+  gek: "693c2832de939b4af8ccd842b17f05df2edd551e59989d3c4ef9a44957b2f1fb",
 };
 
 const pubkeyToHandle = Object.entries(names).reduce((acc, item) => {
