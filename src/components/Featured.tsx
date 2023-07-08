ebab03002b9408ded089bfeabf7a9af2d0be9520
Alejandro Gomez <alejandro@strike.me>
2023-07-08 11:04:24 +0200

    renew featured

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -33,6 +33,19 @@ function WelcomeToNostr() {
   );
 }
 
+function HablaFeatures() {
+  return (
+    <Featured
+      naddr={
+        "naddr1qq8xsctzd3sj6en9v9682un9wvq32amnwvaz7tmjv4kxz7fwv3sk6atn9e5k7tcpr4mhxue69uhh2mnfwejhyum99ehx7um5wf5kx6pwd3skuep0qyghwumn8ghj7mn0wd68ytnhd9hx2tcpramhxue69uhkummnw3ez6un9d3shjtnwda4k7arpwfhjucm0d5hszxthwden5te0dehhxarj94mk7unvvshxsvm69e48qtcpr9mhxue69uhhyetvv9ujuumwdae8gtnnda3kjctv9uq35amnwvaz7tmjv4kxz7fwdehhxarjd93kstnvv9hxgtcpzamhxue69uhhyetvv9ujumn0wd68ytnzv9hxgtcppemhxue69uhkummn9ekx7mp0qgs87hptfey2p607ef36g6cnekuzfz05qgpe34s2ypc2j6x24qvdwhgrqsqqqa28q0jrdf"
+      }
+      kind={30023}
+      pubkey="7f5c2b4e48a0e9feca63a46b13cdb82489f4020398d60a2070a968caa818d75d"
+      identifier="habla-features"
+    />
+  );
+}
+
 function NativeInternetProtocolForSocialMedia() {
   return (
     <Featured
@@ -42,6 +55,17 @@ function NativeInternetProtocolForSocialMedia() {
       kind={30023}
       pubkey="82341f882b6eabcd2ba7f1ef90aad961cf074af15b9ef44a09f9d2a8fbfbe6a2"
       identifier="1681236782798"
+    />
+  );
+}
+
+function NostrTree() {
+  return (
+    <Featured
+      naddr="naddr1qqyrzv3kvvukzvpjqgstnem9g6aqv3tw6vqaneftcj06frns56lj9q470gdww228vysz8hqrqsqqqa28qy2hwumn8ghj7un9d3shjtnyv9kh2uewd9hj7qgawaehxw309a6ku6tkv4e8xefwdehhxarjd93kstnvv9hxgtcpz9mhxue69uhkummnw3ezuamfdejj7qglwaehxw309ahx7um5wgkhyetvv9ujumn0ddhhgctjduhxxmmd9uq3jamnwvaz7tmwdaehgu3dwahhymry9e5rx73wdfcz7qgewaehxw309aex2mrp0yh8xmn0wf6zuum0vd5kzmp0qyd8wumn8ghj7un9d3shjtnwdaehgunfvd5zumrpdejz7qghwaehxw309aex2mrp0yhxummnw3ezucnpdejz7qgwwaehxw309ahx7uewd3hkctc2ke2j0"
+      kind={30023}
+      pubkey="b9e76546ba06456ed301d9e52bc49fa48e70a6bf2282be7a1ae72947612023dc"
+      identifier="126c9a02"
     />
   );
 }
@@ -60,9 +84,9 @@ function PurpleText() {
 }
 
 const featuredAuthors = [
-  "7f5c2b4e48a0e9feca63a46b13cdb82489f4020398d60a2070a968caa818d75d",
   "6e468422dfb74a5738702a8823b9b28168abab8655faacb6853cd0ee15deee93",
-  "5df413d4c5e5035ff508fd99b38b21ea9a0ac0b9ecc34f3312aba9aa2add4f5b",
+  "b9e76546ba06456ed301d9e52bc49fa48e70a6bf2282be7a1ae72947612023dc",
+  "7f5c2b4e48a0e9feca63a46b13cdb82489f4020398d60a2070a968caa818d75d",
 ];
 
 export function FeaturedAuthors() {
@@ -94,9 +118,9 @@ export default function FeaturedArticles() {
     <>
       <SectionHeading>{t("popular-posts")}</SectionHeading>
       <Stack spacing={4}>
-        <WelcomeToNostr />
         <PurpleText />
-        <NativeInternetProtocolForSocialMedia />
+        <NostrTree />
+        <HablaFeatures />
       </Stack>
     </>
   );
