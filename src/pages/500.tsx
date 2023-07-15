46816c7d1d3cce28c1eccb9e3a21fa57905639b6
Alejandro Gomez <alejandro@strike.me>
2023-07-15 08:16:40 +0200

    Revert "tmp: default error pages"
    This reverts commit 6cc1b0dd41c707cf6bf53905c515571e192db2b7.

---
+++ src/pages/500.tsx
@@ -0,0 +1,49 @@
+import Head from "next/head";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { Heading, Flex, Box, Img, Text } from "@chakra-ui/react";
+
+export default function ServerError() {
+  const { t } = useTranslation("common");
+  return (
+    <>
+      <Head>
+        <title>{t("oops")}</title>
+        <meta name="og:title" content={t("habla")} />
+        <meta name="og:description" content={t("tagline")} />
+      </Head>
+      <Flex flexDir="column" px={4} alignItems="center">
+        <Flex
+          flexDir="column"
+          gap={5}
+          maxWidth={["100%", "100%", "48rem"]}
+          width="100%"
+          mt={5}
+        >
+          <Heading>{t("server-error")}</Heading>
+          <Text>{t("server-error-long")}</Text>
+          <Box
+            boxSize={{
+              base: "xs",
+              sm: "sm",
+              md: "md",
+              lg: "xl",
+            }}
+            margin="0 auto"
+            mt={10}
+          >
+            <Img src="/family.png" alt={t("oops")} />
+          </Box>
+        </Flex>
+      </Flex>
+    </>
+  );
+}
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  };
+}
