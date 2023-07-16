c1be674696a7875b0f1b0631beebf164af58d24c
Alejandro Gomez <alejandro@strike.me>
2023-07-16 10:18:02 +0200

    fix: use seed for deterministic placeholders

--- src/components/nostr/Avatar.tsx
+++ src/components/nostr/Avatar.tsx
@@ -1,11 +1,21 @@
+import { useMemo } from "react";
 import { Avatar as BaseAvatar } from "@chakra-ui/react";
 
 import { useUser } from "@habla/nostr/hooks";
 import { useRandomAvatar } from "@habla/hooks/useRandomAvatar";
 
+function hexToDecimal(hexString) {
+  const decimalValue = BigInt("0x" + hexString);
+  const maxValue = BigInt(
+    "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
+  );
+  return Number(decimalValue) / Number(maxValue);
+}
+
 export default function Avatar({ pubkey, size = "sm", ...rest }) {
   const user = useUser(pubkey);
-  const placeholder = useRandomAvatar();
+  const seed = useMemo(() => hexToDecimal(pubkey), [pubkey]);
+  const placeholder = useRandomAvatar(seed);
   return (
     <BaseAvatar
       name={user?.name || pubkey}
--- src/hooks/useRandomAvatar.tsx
+++ src/hooks/useRandomAvatar.tsx
@@ -1,16 +1,16 @@
 import { useMemo } from "react";
 
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomNumber(seed, min, max) {
+  return Math.floor(seed * (max - min + 1)) + min;
 }
 
 function formatTwoDigits(number) {
   return number.toLocaleString("en-US", { minimumIntegerDigits: 2 });
 }
 
-export function useRandomAvatar() {
+export function useRandomAvatar(seed) {
   const n = useMemo(() => {
-    return formatTwoDigits(getRandomNumber(1, 20));
+    return formatTwoDigits(getRandomNumber(seed, 1, 20));
   }, []);
 
   return `/avatars/placeholder_${n}.png`;
