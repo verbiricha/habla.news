61d81533c6e1d224f21bee2c4e787c841cf81677
Nicky Dev <passapol.e@mapboss.co.th>
2023-10-03 09:23:36 +0700

    Removed meta viewport

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -66,21 +66,13 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, []);
 
   return (
-    <>
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-        />
-      </Head>
-      <NostrContext.Provider value={{ ndk }}>
-        <ChakraProvider theme={theme} colorModeManager={colorModeManager}>
-          <QueryClientProvider client={queryClient}>
-            <Component {...pageProps} />
-          </QueryClientProvider>
-        </ChakraProvider>
-      </NostrContext.Provider>
-    </>
+    <NostrContext.Provider value={{ ndk }}>
+      <ChakraProvider theme={theme} colorModeManager={colorModeManager}>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </ChakraProvider>
+    </NostrContext.Provider>
   );
 }
 
