db3b030f13791648bee10104812417d1f25aec19
verbiricha <bandarra+gith@protonmail.com>
2023-10-04 12:26:19 +0200

    chore: upgrade zapthreads

--- src/components/nostr/Thread.tsx
+++ src/components/nostr/Thread.tsx
@@ -10,21 +10,23 @@ export default function Thread({ anchor }) {
   const [loggedInPubkey] = useAtom(pubkeyAtom);
   const [defaultRelays] = useAtom(relaysAtom);
   const npub = useMemo(() => {
-    return loggedInPubkey ? nip19.npubEncode(loggedInPubkey) : '';
+    return loggedInPubkey ? nip19.npubEncode(loggedInPubkey) : "";
   }, [loggedInPubkey]);
 
-  return <zap-threads
-    anchor={anchor}
-    npub={npub}
-    relays={defaultRelays}
-    disable="likes: true, zaps: true"
-  />;
+  return (
+    <zap-threads
+      anchor={anchor}
+      npub={npub}
+      relays={defaultRelays}
+      disable="likes,zaps"
+    />
+  );
 }
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      'zap-threads': ZapThreadsAttributes;
+      "zap-threads": ZapThreadsAttributes;
     }
   }
 }
